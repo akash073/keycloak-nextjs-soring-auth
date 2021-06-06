3fa710bd5a2ad4eb7094f301df12221545522e20
SyedAkash <mahmud.hasan@dsinnovators.com>
2021-06-06 17:27:24 +0600

    keycloak added with role based auth

--- backend/src/main/java/com/dsi/demo/config/CORSFilter.java
+++ backend/src/main/java/com/dsi/demo/config/CORSFilter.java
@@ -21,7 +21,7 @@ public class CORSFilter implements Filter {
          response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, PATCH, DELETE, OPTIONS");
          response.setHeader("Access-Control-Allow-Headers", "origin, content-type, accept, authorization, x-requested-with, X-AUTH-TOKEN, access_token, client_id, device_id, credential");
          response.setHeader("Access-Control-Max-Age", "3600");
-         response.setHeader("Access-Control-Expose-Headers", "authorization, filename, location");
+         response.setHeader("Access-Control-Expose-Headers", "Authorization, filename, location");
          if ("OPTIONS".equalsIgnoreCase(((HttpServletRequest) request).getMethod())) {
               response.setStatus(200);
          } else {
--- backend/src/main/java/com/dsi/demo/controller/StudentController.java
+++ backend/src/main/java/com/dsi/demo/controller/StudentController.java
@@ -1,6 +1,9 @@
 package com.dsi.demo.controller;
 
 import org.springframework.http.MediaType;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +16,10 @@ import javax.annotation.security.DeclareRoles;
 public class StudentController {
 
     @GetMapping("/")
+    @Secured("ROLE_STUDENT")
     public String hello(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentPrincipalName = authentication.getName();
         return "Hello student";
     }
 }
