d268f5a979d4e77c1fd42ba4aa80d0127fb9f7c8
SyedAkash <mahmud.hasan@dsinnovators.com>
2021-06-07 13:05:55 +0600

    Sample project added

---
+++ spring-oauth2-keycloak-login/src/main/java/com/dsi/oauth2/Main.java
@@ -0,0 +1,13 @@
+package com.dsi.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        SpringApplication.run(Main.class, args);
+    }
+}
